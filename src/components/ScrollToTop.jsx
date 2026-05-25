931c7f9c234b0003911821c80c86e61247d471d1
Bishal Gupta <guptabishal111@gmail.com>
2026-05-25 11:31:44 +0530

    swiping bug fix

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,15 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 function ScrollToTop() {
-  const { pathname } = useLocation()
+  const location = useLocation()
 
   useEffect(() => {
+    // location.key is unique per navigation (even same pathname with different state)
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
     window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
-  }, [pathname])
+  }, [location.key])
 
   return null
 }
